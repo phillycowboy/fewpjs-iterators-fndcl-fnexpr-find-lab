180a8f3c9b9770a44d7adfcefc357e206d47dc41
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-04-26 13:11:54 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+function superbowlWin(array) {
+   let year = array.find(res => res.result === 'W')
+    if (year) {
+        return year.year
+    }else{
+        return undefined
+    }
+
+}
+// superbowlWin()
 
